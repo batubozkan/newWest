8eb9b582b594534248788d76da092e9a76634b07
Batuhan Bozkan <batubozkan@gmail.com>
2015-12-12 17:02:32 +0200

    Prototyping Arm Computer
    Using canvas to make a GUI

---
+++ Assets/Scripts/cypherScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class cypherScript : MonoBehaviour
+{
+
+    InputField input;
+    InputField.SubmitEvent se;
+    public Text output;
+
+    void Start()
+    {
+        input = gameObject.GetComponent<InputField>();
+        se = new InputField.SubmitEvent();
+        se.AddListener(SubmitInput);
+        input.onEndEdit = se;
+        input.Select();
+        input.ActivateInputField();
+
+    }
+
+    private void SubmitInput(string arg0)
+    {
+        string currentText = output.text; //maybe add ToString()?
+        string newText = currentText + "\n" + arg0;
+        output.text = newText;
+        input.text = "";
+        input.ActivateInputField();
+    }
+
+
+}
